51d1a925cbd5558592466bee24f3fb7513866f02
oscarruiz05 <oscarruiz2614@gmail.com>
2021-11-16 10:30:02 -0500

    first commit

---
+++ resources/views/layouts/src/head.blade.php
@@ -0,0 +1,40 @@
+<head>
+
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<title>Hotel Sarita |  @yield('title')</title>
+	<meta name="description"  content="" />
+	<meta name="author" content="IG Design">
+	<meta name="keywords"  content="" />
+	<!-- Mobile Specific Metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="theme-color" content="#212121"/>
+    <meta name="msapplication-navbutton-color" content="#212121"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="#212121"/>
+
+	<!-- Web Fonts
+	================================================== -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+
+	<!-- CSS
+	================================================== -->
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/ionicons.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/datepicker.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/jquery.fancybox.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/owl.carousel.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/owl.transitions.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}"/>
+	<link rel="stylesheet" href="{{asset('assets/css/colors/color.css')}}"/>
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="icon" type="image/png" href="favicon.png">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+</head>
---
+++ resources/views/layouts/src/scripts.blade.php
@@ -0,0 +1,7 @@
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/js/popper.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/plugins.js')}}"></script>
+<script src="{{asset('assets/js/flip-slider.js')}}"></script>
+<script src="{{asset('assets/js/reveal-home.js')}}"></script>
+<script src="{{asset('assets/js/custom.js')}}"></script>
