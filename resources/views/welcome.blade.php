0b2204c0467e77403116cbbca228d0c976530662
DiegoJaramisho <diegojaramillo473@gmail.com>
2021-11-25 17:55:17 -0500

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -202,14 +202,14 @@
 			</div>
 		</div>
 	</div>
-
-	<div class="section background-dark over-hide">
+    {{-- mas servicios --}}
+	{{-- <div class="section background-dark over-hide">
 		<div class="container-fluid py-4">
 			<div class="row">
 				<div class="col-sm-6 col-lg-3">
 					<a href="services.html">
 						<div class="img-wrap services-wrap">
-							<img src="img/ser-1.jpg" alt="">
+							<img src="{{asset('assets/img/ser-1.jpg')}}" alt="">
 							<div class="services-text-over">spa salon</div>
 						</div>
 					</a>
@@ -217,7 +217,7 @@
 				<div class="col-sm-6 col-lg-3 pt-4 pt-sm-0">
 					<a href="services.html">
 						<div class="img-wrap services-wrap">
-							<img src="img/ser-2.jpg" alt="">
+							<img src="{{asset('assets/img/ser-2.jpg')}}" alt="">
 							<div class="services-text-over">restaurant</div>
 						</div>
 					</a>
@@ -225,7 +225,7 @@
 				<div class="col-sm-6 col-lg-3 pt-4 pt-lg-0">
 					<a href="services.html">
 						<div class="img-wrap services-wrap">
-							<img src="img/ser-3.jpg" alt="">
+							<img src="{{asset('assets/img/ser-3.jpg')}}" alt="">
 							<div class="services-text-over">pool</div>
 						</div>
 					</a>
@@ -233,14 +233,14 @@
 				<div class="col-sm-6 col-lg-3 pt-4 pt-lg-0">
 					<a href="services.html">
 						<div class="img-wrap services-wrap">
-							<img src="img/ser-4.jpg" alt="">
+							<img src="{{asset('assets/img/ser-4.jpg')}}" alt="">
 							<div class="services-text-over">activities</div>
 						</div>
 					</a>
 				</div>
 			</div>
 		</div>
-	</div>
+	</div> --}}
 
 	<div class="section padding-top-bottom over-hide">
 		<div class="container">
@@ -253,23 +253,23 @@
 				<div class="col-sm-6 col-lg-4">
 					<div class="services-box text-center">
 						<img src="{{asset('assets/img/1.svg')}}" alt="">
-						<h5 class="mt-2">smoking free</h5>
+						<h5 class="mt-2">Libre de Humo</h5>
 						<p class="mt-3">Sed ut perspiciatis unde omnis iste natus error sit, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et.</p>
 						<a class="mt-1 btn btn-primary" href="services.html">read more</a>
 					</div>
 				</div>
 				<div class="col-sm-6 col-lg-4 mt-5 mt-sm-0">
 					<div class="services-box text-center">
-						<img src="{{asset('assets/img/1.svg')}}" alt="">
-						<h5 class="mt-2">king beds</h5>
+						<img src="{{asset('assets/img/2.svg')}}" alt="">
+						<h5 class="mt-2">Zona de estar</h5>
 						<p class="mt-3">Sed ut perspiciatis unde omnis iste natus error sit, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et.</p>
 						<a class="mt-1 btn btn-primary" href="services.html">read more</a>
 					</div>
 				</div>
 				<div class="col-sm-6 col-lg-4 mt-5 mt-lg-0">
 					<div class="services-box text-center">
-						<img src="{{asset('assets/img/1.svg')}}" alt="">
-						<h5 class="mt-2">Yacht rental</h5>
+						<img src="{{asset('assets/img/6.svg')}}" alt="">
+						<h5 class="mt-2">Restaurante</h5>
 						<p class="mt-3">Sed ut perspiciatis unde omnis iste natus error sit, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et.</p>
 						<a class="mt-1 btn btn-primary" href="services.html">read more</a>
 					</div>
@@ -359,18 +359,19 @@
 							<i class="fa fa-star"></i>
 							<i class="fa fa-star"></i>
 						</div>
-						<img src="img/room3.jpg" alt="">
+						<img src="{{asset('assets/img/room4.jpg')}}" alt="">
 						<div class="room-box-in">
 							<h5 class="">pool suite</h5>
 							<p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et.</p>
 							<a class="mt-1 btn btn-primary" href="rooms-gallery.html">book from 130$</a>
-							<div class="room-icons mt-4 pt-4">
-								<img src="img/5.svg" alt="">
-								<img src="img/2.svg" alt="">
-								<img src="img/3.svg" alt="">
-								<img src="img/1.svg" alt="">
+                            {{-- iconos de servicios --}}
+							{{-- <div class="room-icons mt-4 pt-4">
+								<img src="{{asset('assets/img/5.svg')}}" alt="">
+								<img src="{{asset('assets/img/2.svg')}}" alt="">
+								<img src="{{asset('assets/img/3.svg')}}" alt="">
+								<img src="{{asset('assets/img/1.svg')}}" alt="">
 								<a href="rooms-gallery.html">full info</a>
-							</div>
+							</div> --}}
 						</div>
 					</div>
 				</div>
@@ -384,18 +385,19 @@
 							<i class="fa fa-star"></i>
 							<i class="fa fa-star-o"></i>
 						</div>
-						<img src="img/room4.jpg" alt="">
+						<img src="{{asset('assets/img/room4.jpg')}}" alt="">
 						<div class="room-box-in">
 							<h5 class="">small room</h5>
 							<p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et.</p>
 							<a class="mt-1 btn btn-primary" href="rooms-gallery.html">book from 80$</a>
-							<div class="room-icons mt-4 pt-4">
-								<img src="img/4.svg" alt="">
-								<img src="img/2.svg" alt="">
-								<img src="img/6.svg" alt="">
-								<img src="img/3.svg" alt="">
+                            {{-- iconos de servicios --}}
+							{{-- <div class="room-icons mt-4 pt-4">
+								<img src="{{asset('assets/img/4.svg')}}" alt="">
+								<img src="{{asset('assets/img/2.svg')}}" alt="">
+								<img src="{{asset('assets/img/6.svg')}}" alt="">
+								<img src="{{asset('assets/img/3.svg')}}" alt="">
 								<a href="rooms-gallery.html">full info</a>
-							</div>
+							</div> --}}
 						</div>
 					</div>
 				</div>
@@ -409,18 +411,19 @@
 							<i class="fa fa-star"></i>
 							<i class="fa fa-star"></i>
 						</div>
-						<img src="img/room5.jpg" alt="">
+						<img src="{{asset('assets/img/room4.jpg')}}" alt="">
 						<div class="room-box-in">
 							<h5 class="">Apartment</h5>
 							<p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et.</p>
 							<a class="mt-1 btn btn-primary" href="rooms-gallery.html">book from 110$</a>
-							<div class="room-icons mt-4 pt-4">
+                            {{-- iconos servicios --}}
+							{{-- <div class="room-icons mt-4 pt-4">
 								<img src="img/5.svg" alt="">
 								<img src="img/2.svg" alt="">
 								<img src="img/3.svg" alt="">
 								<img src="img/6.svg" alt="">
 								<a href="rooms-gallery.html">full info</a>
-							</div>
+							</div> --}}
 						</div>
 					</div>
 				</div>
@@ -434,18 +437,19 @@
 							<i class="fa fa-star"></i>
 							<i class="fa fa-star"></i>
 						</div>
-						<img src="img/room6.jpg" alt="">
+						<img src="{{asset('assets/img/room4.jpg')}}" alt="">
 						<div class="room-box-in">
 							<h5 class="">big Apartment</h5>
 							<p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et.</p>
 							<a class="mt-1 btn btn-primary" href="rooms-gallery.html">book from 160$</a>
-							<div class="room-icons mt-4 pt-4">
+                            {{-- iconos servicios --}}
+							{{-- <div class="room-icons mt-4 pt-4">
 								<img src="img/5.svg" alt="">
 								<img src="img/2.svg" alt="">
 								<img src="img/3.svg" alt="">
 								<img src="img/6.svg" alt="">
 								<a href="rooms-gallery.html">full info</a>
-							</div>
+							</div> --}}
 						</div>
 					</div>
 				</div>
