ac289ea7450d22e10b2ead5936acf1bbd6a9a8bf
DiegoJaramisho <diegojaramillo473@gmail.com>
2021-12-27 17:59:27 -0500

    a

--- resources/views/layouts/src/scripts.blade.php
+++ resources/views/layouts/src/scripts.blade.php
@@ -1,7 +1,7 @@
-<script src="{{asset('assets/js/jquery.min.js')}}"></script>
-<script src="{{asset('assets/js/popper.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('assets/js/plugins.js')}}"></script>
-<script src="{{asset('assets/js/flip-slider.js')}}"></script>
-<script src="{{asset('assets/js/reveal-home.js')}}"></script>
-<script src="{{asset('assets/js/custom.js')}}"></script>
+<script src="{{secure_asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/popper.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{secure_asset('assets/js/plugins.js')}}"></script>
+<script src="{{secure_asset('assets/js/flip-slider.js')}}"></script>
+<script src="{{secure_asset('assets/js/reveal-home.js')}}"></script>
+<script src="{{secure_asset('assets/js/custom.js')}}"></script>
